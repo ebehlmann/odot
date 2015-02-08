af1e0fd8919e8dae9d59a3c05e608eb627de5ddc
Emily Behlmann <ebehlmann@gmail.com>
2015-02-07 19:08:50 -0600

    added specs and code for editing todo items

---
+++ spec/features/todo_items/edit_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe "Editing todo items" do
+	let!(:todo_list) { TodoList.create(title: "Groceries list", description: "Groceries to buy") }
+	let!(:todo_item) { todo_list.todo_items.create(content: "Milk") }
+
+	def visit_todo_list(list)
+		visit "/todo_lists"
+		within "#todo_list_#{list.id}" do
+			click_link "List Items"
+		end
+	end
+
+	it "is successful with valid content" do
+		visit_todo_list(todo_list)
+		within("#todo_item_#{todo_item.id}") do
+			click_link "Edit"
+		end
+		fill_in "Content", with: "Lots of milk"
+		click_button "Save"
+		expect(page).to have_content("Updated todo list item")
+		todo_item.reload
+		expect(todo_item.content).to eq("Lots of milk")
+	end
+
+	it "is fails with no content" do
+		visit_todo_list(todo_list)
+		within("#todo_item_#{todo_item.id}") do
+			click_link "Edit"
+		end
+		fill_in "Content", with: ""
+		click_button "Save"
+		expect(page).to have_content("That didn't work")
+		todo_item.reload
+		expect(todo_item.content).to eq("Milk")
+	end
+
+	it "is fails if content is too short" do
+		visit_todo_list(todo_list)
+		within("#todo_item_#{todo_item.id}") do
+			click_link "Edit"
+		end
+		fill_in "Content", with: "a"
+		click_button "Save"
+		expect(page).to have_content("That didn't work")
+		todo_item.reload
+		expect(todo_item.content).to eq("Milk")
+	end
+
+end
